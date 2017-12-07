68f45383299633f01844369d8ced3a71cb04b73d
Austin Shalit <austinshalit@gmail.com>
2017-12-07 15:00:27 -0500

    init commit

---
+++ devel/share/robot_controller/cmake/robot_controller-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(robot_controller_MESSAGE_FILES "")
+set(robot_controller_SERVICE_FILES "")
---
+++ src/robot_controller/src/robot_controller.cpp
@@ -0,0 +1,104 @@
+
+#include <ros/ros.h>
+
+#include <tf/transform_broadcaster.h>
+#include <std_msgs/UInt8.h>
+#include <std_msgs/Float32.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+uint8_t state = 0;
+
+MoveBaseClient* ac;
+
+void flameHAngleCallback(const std_msgs::Float32::ConstPtr& msg) {
+  if (msg->data != -1.0 && state != 3) {
+    state = 3;
+
+    move_base_msgs::MoveBaseGoal goal;
+
+    goal.target_pose.header.frame_id = "base_link";
+    goal.target_pose.header.stamp = ros::Time::now();
+
+    goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(msg->data);
+
+    ac->sendGoal(goal);
+  } else if (state == 3) {
+    ac->cancelAllGoals();
+  }
+}
+
+void stateCallback(const std_msgs::UInt8::ConstPtr& msg) {
+  state = msg->data; 
+}
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "robot_controller");
+  ros::NodeHandle n;
+
+  ac = new MoveBaseClient("move_base", true);
+
+  while(!ac->waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+
+  ros::Publisher state_pub = n.advertise<std_msgs::UInt8>("robot_state", 1000);
+  ros::Subscriber state_sub = n.subscribe("robot_state", 1000, stateCallback);
+  ros::Subscriber flameh_angle_sub = n.subscribe("flameh_angle", 1000, flameHAngleCallback);
+
+  ros::Rate loop_rate(10);
+
+  while(ros::ok()) {
+    switch (state) {
+      case 0: {
+        // do nothing
+        break;
+      }
+      case 1: {
+        // explore node
+        break;
+      }
+      case 2: {
+        // check for flame (turn in place)
+        //if ()
+        break;
+      }
+      case 3: {
+        // calculate flame location
+        if (ac->getState().isDone()) {
+          state = 0;
+        }
+        break;
+      }
+      case 4: {
+        // blow out flame
+        break;
+      }
+      case 5: {
+        // return home
+        move_base_msgs::MoveBaseGoal goal;
+
+        goal.target_pose.header.frame_id = "map";
+        goal.target_pose.header.stamp = ros::Time::now();
+
+        goal.target_pose.pose.position.x = 0.0;
+        goal.target_pose.pose.position.y = 0.0;
+        goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(0);
+
+        ac->sendGoal(goal);
+
+        if (ac->getState().isDone()) {
+          state = 0;
+        }
+        break;
+      }
+    }
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  return 0;
+}
