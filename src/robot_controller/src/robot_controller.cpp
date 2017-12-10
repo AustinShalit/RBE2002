85365838f9c1fa47a3ec8e76c0979d2e57a39a0f
Austin Shalit <austinshalit@gmail.com>
2017-12-10 13:53:51 -0500

    Used state changes for control

--- src/robot_controller/src/robot_controller.cpp
+++ src/robot_controller/src/robot_controller.cpp
@@ -2,6 +2,7 @@
 #include <ros/ros.h>
 
 #include <tf/transform_broadcaster.h>
+#include <geometry_msgs/Twist.h>
 #include <std_msgs/UInt8.h>
 #include <std_msgs/Float32.h>
 #include <move_base_msgs/MoveBaseAction.h>
@@ -13,9 +14,9 @@ uint8_t state = 0;
 
 MoveBaseClient* ac;
 
-void emptyCallback() {
-
-}
+/*************
+ * Callbacks *
+ *************/
 
 void flameHAngleCallback(const std_msgs::Float32::ConstPtr& msg) {
   if (msg->data != -1.0 && state != 3) {
@@ -34,64 +35,16 @@ void flameHAngleCallback(const std_msgs::Float32::ConstPtr& msg) {
   }
 }
 
-void turnInPlaceDoneCallback(const actionlib::SimpleClientGoalState& state, const ResultConstPtr& result) {
-  state = 2;
-}
-
 void stateCallback(const std_msgs::UInt8::ConstPtr& msg) {
   state = msg->data;
+}
 
-  switch (state) {
-      case 0: {
-        // do nothing
-        break;
-      }
-      case 1: {
-        // explore node
-        break;
-      }
-      case 2: {
-        // check for flame (turn in place)
-        goal.target_pose.header.frame_id = "base_link";
-        goal.target_pose.header.stamp = ros::Time::now();
-        for (int i = 1; i <= 3; i++) {
-          goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(i * 0.785);
-          ac->sendGoal(goal);
-        }
-        goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(4 * 0.785);
-        ac->sendGoal(goal, &doneCb, &emptyCallback, &emptyCallback);
-        break;
-      }
-      case 3: {
-        // calculate flame location
-        if (ac->getState().isDone()) {
-          state = 0;
-        }
-        break;
-      }
-      case 4: {
-        // blow out flame
-        break;
-      }
-      case 5: {
-        // return home
-        move_base_msgs::MoveBaseGoal goal;
-
-        goal.target_pose.header.frame_id = "map";
-        goal.target_pose.header.stamp = ros::Time::now();
-
-        goal.target_pose.pose.position.x = 0.0;
-        goal.target_pose.pose.position.y = 0.0;
-        goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(0);
-
-        ac->sendGoal(goal);
-
-        if (ac->getState().isDone()) {
-          state = 0;
-        }
-        break;
-      }
-    }
+void turnLeft() {
+  move_base_msgs::MoveBaseGoal goal;
+  goal.target_pose.header.frame_id = "base_link";
+  goal.target_pose.header.stamp = ros::Time::now();
+  goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(2.09);
+  ac->sendGoal(goal);
 }
 
 int main(int argc, char** argv){
@@ -111,11 +64,74 @@ int main(int argc, char** argv){
   ros::Rate loop_rate(10);
 
   std_msgs::UInt8 stateMessage;
+  bool stateChanged = true;
 
   while(ros::ok()) {
+    switch (state) {
+      case 0: {
+        // do nothing
+        break;
+      }
+      case 1: {
+        // explore node
+        break;
+      }
+      case 2: {
+        // check for flame (turn in place) 1/3
+        if (stateChanged) {
+          turnLeft();
+        } else if (ac->getState().isDone()) {
+          state++;
+        }
+        break;
+      }
+      case 3: {
+        // check for flame (turn in place) 2/3
+        if (stateChanged) {
+          turnLeft();
+        } else if (ac->getState().isDone()) {
+          state++;
+        }
+        break;
+      }
+      case 4: {
+        // check for flame (turn in place) 3/3
+        if (stateChanged) {
+          turnLeft();
+        } else if (ac->getState().isDone()) {
+          state = 1;
+        }
+        break;
+      }
+      case 5: {
+        // calculate flame location
+        break;
+      }
+      case 6: {
+        // blow out flame
+        break;
+      }
+      case 7: {
+        // return home
+        if (stateChanged) {
+          move_base_msgs::MoveBaseGoal goal;
+
+          goal.target_pose.header.frame_id = "map";
+          goal.target_pose.header.stamp = ros::Time::now();
+
+          ac->sendGoal(goal);
+        } else if (ac->getState().isDone()) {
+          state = 0;
+        }
+        break;
+      }
+    }
 
     if (stateMessage.data != state) {
       state_pub.publish(stateMessage);
+      stateChanged = true;
+    } else {
+      stateChanged = false;
     }
 
     ros::spinOnce();
